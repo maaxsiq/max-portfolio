f8f44c31aa4bcac579f363e449b09b88121570de
Maxwell Siqueira <56099524+maaxsiq@users.noreply.github.com>
2025-12-02 00:05:45 -0300

    update

---
+++ tailwind.config.ts
@@ -0,0 +1,9 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default {
+  content: [
+    "./src/app/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {},
+  plugins: [],
+};
